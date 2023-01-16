11c9693d018e549c1832cfd6da5beafa33995a93
Keon <h21816595@gmail.com>
2023-01-16 13:15:01 +0800

    no message

--- JS/js.js
+++ JS/js.js
@@ -2,11 +2,14 @@ let header = document.querySelector("header");
 let navbar = document.querySelectorAll(".header_nav li a");
 window.addEventListener("scroll",()=>{
     if(window.pageYOffset !=0){
-        header.style="background:black;color:white";
-        navbar.forEach(a=>{
-            a.style.color="while"
+        header.style="background:rgba(0,0,0,0.3);color:white";
+        navbar.forEach(a =>{
+            a.style.color = "white";
         })
     }else{
         header.style="";
+        navbar.forEach(a =>{
+            a.style.color = "";
+        })
     }
 })
